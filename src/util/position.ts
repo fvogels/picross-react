e1887c31945ef1aad7fd8984b41f1a769423f982
Frederic Vogels <frederic.vogels@gmail.com>
2025-11-22 00:17:28 +0100

    Position

---
+++ src/util/position.ts
@@ -0,0 +1,21 @@
+export class Position
+{
+    private readonly _x: number;
+    private readonly _y: number;
+
+    constructor(x: number, y: number)
+    {
+        this._x = x;
+        this._y = y;
+    }
+
+    public get x(): number
+    {
+        return this._x;
+    }
+
+    public get y(): number
+    {
+        return this._y;
+    }
+}
