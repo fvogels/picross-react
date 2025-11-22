cd925b829d5d557ec60f9ef831d23988e61f0bd4
Frederic Vogels <frederic.vogels@gmail.com>
2025-11-22 15:09:58 +0100

    PersistentArray

---
+++ src/util/parray.ts
@@ -0,0 +1,35 @@
+import { range } from ".";
+
+export class PersistentArray<T>
+{
+    private readonly items: T[];
+
+    static create<T>(size: number, initializer: (index: number) => T): PersistentArray<T>
+    {
+        const items = range(0, size).map(initializer);
+
+        return new PersistentArray<T>(items);
+    }
+
+    private constructor(items: T[])
+    {
+        this.items = items;
+    }
+
+    get length(): number
+    {
+        return this.items.length;
+    }
+
+    at(index: number): T
+    {
+        return this.items[index];
+    }
+
+    update(index: number, newValue: T): PersistentArray<T>
+    {
+        const copy = [...this.items];
+        copy[index] = newValue;
+        return new PersistentArray<T>(copy);
+    }
+}
