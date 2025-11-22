cdf569488895f98d394e2838471eea08243ab51d
Frederic Vogels <frederic.vogels@gmail.com>
2025-11-22 01:06:52 +0100

    createSelectionIndexer

--- src/util/index.ts
+++ src/util/index.ts
@@ -22,4 +22,67 @@ export function indexOf<T>(xs: T[], predicate: (t: T) => boolean): number | null
 
     return null;
 }
+
+export function createSelectionIndexer(start: Position, end: Position): (p: Position) => number | null
+{
+    const dx = Math.abs(end.x - start.x);
+    const dy = Math.abs(end.y - start.y);
+
+    if ( dx >= dy )
+    {
+        // Horizontal selection
+        if ( start.x <= end.x )
+        {
+            // Left to right
+            return (p: Position) => {
+                if ( p.y !== start.y || p.x < start.x || p.x > end.x )
+                {
+                    return null;
+                }
+
+                return p.x - start.x;
+            };
+        }
+        else
+        {
+            // Right to left
+            return (p: Position) => {
+                console.log(`R2L ${p} ${start} ${end}`)
+                if ( p.y !== start.y || p.x < end.x || p.x > start.x )
+                {
+                    return null;
+                }
+
+                return start.x - p.x;
+            };
+        }
+    }
+    else
+    {
+        // Vertical selection
+        if ( start.y <= end.y )
+        {
+            // Top to bottom
+            return (p: Position) => {
+                if ( p.x !== start.x || p.y < start.y || p.y > end.y )
+                {
+                    return null;
+                }
+
+                return p.y - start.y;
+            };
+        }
+        else
+        {
+            // Bottom to top
+            return (p: Position) => {
+                if ( p.x !== start.x || p.y < end.y || p.y > start.y )
+                {
+                    return null;
+                }
+
+                return start.y - p.y;
+            };
+        }
+    }
 }
