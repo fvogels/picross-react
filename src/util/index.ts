17e04a498dd8f66d915a215e9c4b1cb1f8ab833d
Frederic Vogels <frederic.vogels@gmail.com>
2025-11-22 01:06:44 +0100

    indexOf

--- src/util/index.ts
+++ src/util/index.ts
@@ -9,3 +9,17 @@ export function range(start: number, stop: number): number[]
 
     return result;
 }
+
+export function indexOf<T>(xs: T[], predicate: (t: T) => boolean): number | null
+{
+    for ( let i = 0; i !== xs.length; ++i )
+    {
+        if ( predicate(xs[i]) )
+        {
+            return i;
+        }
+    }
+
+    return null;
+}
+}
