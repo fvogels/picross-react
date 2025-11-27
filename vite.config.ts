74d43558b90e721d39e7d8f18c336fa3dbe7a168
Frederic Vogels <frederic.vogels@gmail.com>
2025-11-27 10:58:40 +0100

    single file output

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tsconfigPaths from 'vite-tsconfig-paths';
+import { viteSingleFile } from "vite-plugin-singlefile"
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     tsconfigPaths(),
+    viteSingleFile(),
   ],
 })
